56c1941087f4cfe038566c6b06a05adb406bf9d8
chaima.ktari <chaima.ktari@esprit.tn>
2024-07-04 15:08:21 +0100

    initialize project

---
+++ src/main/java/com/example/PetgoraBackend/PetgoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetgoraBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetgoraBackendApplication.class, args);
+	}
+
+}
